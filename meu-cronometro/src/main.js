b29565b541a99736f19b655dd115cf5383aba3f5
Marcelo Oliveira <104630839+heiderick13@users.noreply.github.com>
2025-05-22 00:12:38 +0000

    add button function :construction:

--- meu-cronometro/src/main.js
+++ meu-cronometro/src/main.js
@@ -1,24 +1,8 @@
-import './style.css'
-import javascriptLogo from './javascript.svg'
-import viteLogo from '/vite.svg'
-import { setupCounter } from './counter.js'
+const startBtn = document.querySelector('#startBtn');
+const input = document.querySelector('#inputDuration');
 
-document.querySelector('#app').innerHTML = `
-  <div>
-    <a href="https://vite.dev" target="_blank">
-      <img src="${viteLogo}" class="logo" alt="Vite logo" />
-    </a>
-    <a href="https://developer.mozilla.org/en-US/docs/Web/JavaScript" target="_blank">
-      <img src="${javascriptLogo}" class="logo vanilla" alt="JavaScript logo" />
-    </a>
-    <h1>Hello Vite!</h1>
-    <div class="card">
-      <button id="counter" type="button"></button>
-    </div>
-    <p class="read-the-docs">
-      Click on the Vite logo to learn more
-    </p>
-  </div>
-`
-
-setupCounter(document.querySelector('#counter'))
+startBtn.addEventListener('click', (e) => {
+  let timer = input.value;
+  console.log(timer);
+  
+})
